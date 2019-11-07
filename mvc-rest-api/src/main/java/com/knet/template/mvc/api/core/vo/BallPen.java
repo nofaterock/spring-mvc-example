cd1c132f8cdda6ac8c01dedbbeb5849efae32af0
이승환 <knetdev@knetbiz.com>
2019-11-07 16:06:48 +0900

    api = >  NamePen, Vodmaca, BallPen 클래스 추가

---
+++ mvc-rest-api/src/main/java/com/knet/template/mvc/api/core/vo/BallPen.java
@@ -0,0 +1,18 @@
+package com.knet.template.mvc.api.core.vo;
+
+public class BallPen {
+    private String colorCode;
+    private boolean isButton;
+    
+    BallPen(String colorCode, boolean isButton){
+        this.colorCode = colorCode;
+        this.isButton = isButton;
+    }
+    String getColor(){
+        return colorCode;
+    }
+    boolean onButton() {
+        isButton = !isButton;
+        return isButton;
+    }
+}
